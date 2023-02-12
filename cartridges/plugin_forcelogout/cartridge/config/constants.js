e512ac50032488ad34b709aa27fc2ae117c8ebcc
Marush Denchev <avreon@gmail.com>
2023-02-12 14:49:31 +0200

    Move preference name as constant

--- cartridges/plugin_forcelogout/cartridge/config/constants.js
+++ cartridges/plugin_forcelogout/cartridge/config/constants.js
@@ -8,5 +8,10 @@ const FORCE_LOGOUT_MODES = {
     'PASSWORD_CHANGE': 2
 };
 
+const PREFERENCES = {
+    'FORCE_LOGOUT_MODE': 'forceLogoutMode'
+}
+
 module.exports.MAX_RANDOM_INT=MAX_RANDOM_INT;
 module.exports.FORCE_LOGOUT_MODES = FORCE_LOGOUT_MODES;
+module.exports.PREFERENCES = PREFERENCES;
--- cartridges/plugin_forcelogout/cartridge/scripts/helpers/preferences.js
+++ cartridges/plugin_forcelogout/cartridge/scripts/helpers/preferences.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function getForceLogoutMode() {
+    const constants = require('~/cartridge/config/constants');
     const Site = require('dw/system/Site');
-    const forceLogoutMode = parseInt(Site.getCurrent().getCustomPreferenceValue('forceLogoutMode'), 10);
+    const forceLogoutMode = parseInt(Site.getCurrent().getCustomPreferenceValue(constants.PREFERENCES.FORCE_LOGOUT_MODE), 10);
 
     return forceLogoutMode;
 }
